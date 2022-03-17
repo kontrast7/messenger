a2ebc54c5fe522a5b30c907363a127ff316cdf12
makssmeinart <m.meinarts10@gmail.com>
2022-03-17 18:15:32 +0200

    Eslint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,11 @@
-import axios from "axios";
-
+import axios from "axios"
 
 const instance = axios.create({
-
-    baseURL: "https://socialmess.herokuapp.com/api/",
-
+  baseURL: "https://socialmess.herokuapp.com/api/",
 })
 
 export const authApi = {
-    registerUser:() => {
-        return instance.post("")
-    }
+  registerUser: () => {
+    return instance.post("")
+  },
 }
-
-// ops
