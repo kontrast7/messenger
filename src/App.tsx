632b3901667d5dc4e60e822920dfe2d69f044192
makssmeinart <m.meinarts10@gmail.com>
2022-03-17 19:49:28 +0200

    Added Redux, Reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { usersApi } from "./api/api";
+import { Header } from "./components/header/Header";
+import { Routes } from "react-router-dom";
+import { PageHolder } from "./components/pageholder/PageHolder";
 
 function App() {
   useEffect(() => {
@@ -8,11 +11,14 @@ function App() {
       .getAllUsers("62332a59d327af0023f7dfcf")
       .then((res) => console.log(JSON.stringify(res)));
   }, []);
-//f
+  //f
   return (
-    <div className="App">
-      <header className="App-header">123</header>
-    </div>
+    <Routes>
+      <div className="App">
+        <Header />
+        <PageHolder />
+      </div>
+    </Routes>
   );
 }
 
---
+++ src/components/pageholder/PageHolder.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const PageHolder = () => {
+  return (
+    <div>
+      PageHolder
+    </div>
+  )
+}
+
