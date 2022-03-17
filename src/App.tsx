df05307b63f5b6c5444e365bc9246b3b6b19175c
kontrast7 <maks.mashko@gmail.com>
2022-03-17 19:10:43 +0300

    start api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { usersApi } from "./api/api";
 
 function App() {
+  useEffect(() => {
+    usersApi
+      .getAllUsers("62332a59d327af0023f7dfcf")
+      .then((res) => console.log(JSON.stringify(res)));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">123</header>
