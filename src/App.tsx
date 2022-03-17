f2cc7b57bcc6f77c2f76a7e6d1c8fcd29184cce5
makssmeinart <m.meinarts10@gmail.com>
2022-03-17 19:52:43 +0200

    Added Redux, Reducers

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { usersApi } from "./api/api";
 import { Header } from "./components/header/Header";
 import { Routes } from "react-router-dom";
 import { PageHolder } from "./components/pageholder/PageHolder";
+import { useState } from "react"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   useEffect(() => {
     usersApi
       .getAllUsers("62332a59d327af0023f7dfcf")
       .then((res) => console.log(JSON.stringify(res)));
   }, []);
-  //f
+
   return (
     <Routes>
       <div className="App">
-        <Header />
+        {!isLoggedIn && <Header />}
         <PageHolder />
       </div>
     </Routes>
