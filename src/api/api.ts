784a1fc080da1264272f73f477a6db56b29906ef
kontrast7 <maks.mashko@gmail.com>
2022-03-17 18:44:16 +0300

    start

---
+++ src/api/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    baseURL: "https://socialmess.herokuapp.com/api/",
+
+})
+
+export const authApi = {
+    registerUser:() => {
+        return instance.post("")
+    }
+}
