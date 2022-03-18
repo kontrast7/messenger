ff6630786456c47aa24a7d1b282d1167a66075f0
kontrast7 <maks.mashko@gmail.com>
2022-03-18 21:26:20 +0300

    add mesendger

--- src/bll/redux/store.ts
+++ src/bll/redux/store.ts
@@ -1,11 +1,11 @@
 import { combineReducers, createStore } from "redux";
-import { registerReducer } from "bll/reducer/registerReducer";
-import { loginReducer } from "bll/reducer/loginReducer";
 import { appReducer } from "../reducer/appReducer";
 import { applyMiddleware } from "redux";
 import { usersReducer } from "../reducer/usersReducer";
 import { roomsReducer } from "../reducer/roomsReducer";
 import { messageReducer } from "../reducer/messageReducer";
+import { registerReducer } from "../reducer/registerReducer";
+import { loginReducer } from "../reducer/loginReducer";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
